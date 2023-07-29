9515f768de81a18ca0b68cb05d94adebf011dfd4
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-29 20:34:31 +0530

    Update userbot.py

--- plugins/userbot.py
+++ plugins/userbot.py
@@ -1,57 +1,152 @@
-import logging
 import asyncio
-
-from pyrogram import Client, filters
-from pyrogram.errors import FloodWait
-
-from info import USERBOT_STRING_SESSION, API_ID, API_HASH, ADMINS, id_pattern
+import re
+import logging
+from info import ADMINS
 from utils import save_file
-
+from pyrogram import Client, filters, enums
+from pyrogram.errors import FloodWait
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 logger = logging.getLogger(__name__)
-lock = asyncio.Lock()
+
+CURRENT = {}
+CANCEL = {}
+INDEXING = {}
+
+@Client.on_callback_query(filters.regex(r'^forward'))
+async def index(bot, query):
+    _, ident, chat, lst_msg_id = query.data.split("#")
+    if ident == 'yes':
+        if INDEXING.get(query.from_user.id):
+            return await query.answer('Wait until previous process complete.', show_alert=True)
+
+        msg = query.message
+        await msg.edit('Starting Indexing...')
+        try:
+            chat = int(chat)
+        except:
+            chat = chat
+        await index_files(int(lst_msg_id), chat, msg, bot, query.from_user.id)
+
+    elif ident == 'close':
+        await query.answer("Okay!")
+        await query.message.delete()
+
+    elif ident == 'cancel':
+        await query.message.edit("Trying to cancel forwarding...")
+        CANCEL[query.from_user.id] = True
 
 
-@Client.on_message(filters.command(['index', 'indexfiles']) & filters.user(ADMINS))
-async def index_files(bot, message):
-    """Save channel or group files with the help of user bot"""
-
-    if not USERBOT_STRING_SESSION:
-        await message.reply('Set `USERBOT_STRING_SESSION` in info.py file or in environment variables.')
-    elif len(message.command) == 1:
-        await message.reply('Please specify channel username or id in command.\n\nExample: `/index -10012345678`')
-    elif lock.locked():
-        await message.reply('Wait until previous process complete.')
+@Client.on_message((filters.forwarded | (filters.regex("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text) & filters.private & filters.incoming)
+async def send_for_index(bot, message):
+    if message.from_user.id not in ADMINS:
+        return
+    if message.text:
+        regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
+        match = regex.match(message.text)
+        if not match:
+            return await message.reply('Invalid link for forward!')
+        chat_id = match.group(4)
+        last_msg_id = int(match.group(5))
+        if chat_id.isnumeric():
+            chat_id  = int(("-100" + chat_id))
+    elif message.forward_from_chat.type == enums.ChatType.CHANNEL:
+        last_msg_id = message.forward_from_message_id
+        chat_id = message.forward_from_chat.username or message.forward_from_chat.id
     else:
-        msg = await message.reply('Processing...⏳')
-        raw_data = message.command[1:]
-        user_bot = Client('User-bot', API_ID, API_HASH, session_string=USERBOT_STRING_SESSION)
-        chats = [int(chat) if id_pattern.search(chat) else chat for chat in raw_data]
-        total_files = 0
+        return
 
-        async with lock:
-            try:
-                async with user_bot:
-                    for chat in chats:
+    try:
+        source_chat = await bot.get_chat(chat_id)
+    except Exception as e:
+        return await message.reply(f'Error - {e}')
+    # last_msg_id is same to total messages
+    buttons = [[
+        InlineKeyboardButton('YES', callback_data=f'forward#yes#{chat_id}#{last_msg_id}')
+    ],[
+        InlineKeyboardButton('CLOSE', callback_data=f'forward#close#{chat_id}#{last_msg_id}')
+    ]]
+    await message.reply(f"Source Channel: {source_chat.title}\nTarget Channel: {target_chat.title}\nSkip messages: <code>{skip}</code>\nTotal Messages: <code>{last_msg_id}</code>\nFile Caption: {caption}\n\nDo you want to forward?", reply_markup=InlineKeyboardMarkup(buttons))
 
-                        async for user_message in user_bot.get_chat_history(chat):
-                            try:
-                                message = await bot.get_messages(chat, user_message.id, replies=0)
-                            except FloodWait as e:
-                                await asyncio.sleep(e.value)
-                                message = await bot.get_messages(chat, user_message.id, replies=0)
 
-                            for file_type in ("document", "video", "audio"):
-                                media = getattr(message, file_type, None)
-                                if media is not None:
-                                    break
-                            else:
-                                continue
-                            media.file_type = file_type
-                            media.caption = message.caption
-                            await save_file(media)
-                            total_files += 1
-            except Exception as e:
-                logger.exception(e)
-                await msg.edit(f'Error: {e}')
-            else:
-                await msg.edit(f'Total {total_files} checked!')
+@Client.on_message(filters.private & filters.command(['set_skip']))
+async def set_skip_number(bot, message):
+    try:
+        _, skip = message.text.split(" ")
+    except:
+        return await message.reply("Give me a skip number.")
+    try:
+        skip = int(skip)
+    except:
+        return await message.reply("Only support in numbers.")
+    CURRENT[message.from_user.id] = int(skip)
+    await message.reply(f"Successfully set <code>{skip}</code> skip number.")
+
+
+async def index_files(lst_msg_id, chat, msg, bot, user_id):
+    current = CURRENT.get(user_id) if CURRENT.get(user_id) else 0
+    indexed = 0
+    deleted = 0
+    duplicate = 0
+    errors = 0
+    unsupported = 0
+    fetched = 0
+    CANCEL[user_id] = False  
+    INDEXING[user_id] = True
+    # lst_msg_id is same to total messages
+
+    try:
+        async for message in bot.iter_messages(chat, lst_msg_id, CURRENT.get(user_id) if CURRENT.get(user_id) else 0):
+            if CANCEL.get(user_id):
+                await msg.edit(f"Successfully Forward Canceled!")
+                break
+            current += 1
+            fetched += 1
+            if current % 20 == 0:
+                btn = [[
+                    InlineKeyboardButton('CANCEL', callback_data=f'forward#cancel#{chat}#{lst_msg_id}')
+                ]]
+                await msg.edit_text(
+                    text=f"Index Processing...\n\nTotal Messages: <code>{lst_msg_id}</code>\nCompleted Messages: <code>{current} / {lst_msg_id}</code>\nIndexed Files: <code>{indexed}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nUnsupported Files Skipped: <code>{unsupported}</code>\nDuplicat files skiped: {duplicate}\nError save to files: {errors}", 
+                    reply_markup=InlineKeyboardMarkup(btn)
+                )
+            if message.empty:
+                deleted += 1
+                continue
+            elif not message.media:
+                unsupported += 1
+                continue
+            elif message.media not in [enums.MessageMediaType.DOCUMENT, enums.MessageMediaType.VIDEO]:  # Non documents and videos files skipping
+                unsupported += 1
+                continue
+            media = getattr(message, message.media.value, None)
+            if not media:
+                unsupported += 1
+                continue
+            elif media.mime_type not in ['video/mp4', 'video/x-matroska']:  # Non mp4 and mkv files types skipping
+                unsupported += 1
+                continue
+            media.file_type = message.media.value
+            media.caption = message.caption
+            ab, bc = save_file(media)
+            if ab:
+                total_files += 1
+            elif bc == 0:
+                duplicate += 1
+            elif bc == 2:
+                errors += 1
+    except Exception as e:
+        logger.exception(e)
+        await msg.reply(f"Index Canceled!\n\nError - {e}")
+    else:
+        await msg.edit(f'Index Completed!\n\nTotal Messages: <code>{lst_msg_id}</code>\nCompleted Messages: <code>{current} / {lst_msg_id}</code>\nFetched Messages: <code>{fetched}</code>\nTotal Indexed Files: <code>{indexed}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nUnsupported Files Skipped: <code>{unsupported}</code>\nSkiped duplicate: {duplicate}\nError save to file: {errors}')
+        INDEXING[user_id] = False
+
+
+def get_size(size):
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units):
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
