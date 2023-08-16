d50a8377fef05530d815928c7d52628e2070283a
Protullen <138602811+protullen@users.noreply.github.com>
2023-08-16 10:36:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from info import START_MSG, CHANNELS, ADMINS, INVITE_MSG
-from utils import Media
+from utils import Media, unpack_new_file_id
 
 logger = logging.getLogger(__name__)
 
@@ -79,29 +79,45 @@ async def log_file(bot, message):
 async def delete(bot, message):
     """Delete file from database"""
     reply = message.reply_to_message
-    if not (reply and reply.media):
+    if reply and reply.media:
+        msg = await message.reply("Processing...⏳", quote=True)
+    else:
         await message.reply('Reply to file with /delete which you want to delete', quote=True)
         return
 
-    msg = await message.reply("Processing...⏳", quote=True)
-
     for file_type in ("document", "video", "audio"):
         media = getattr(reply, file_type, None)
-        if media:
-            media.file_type = file_type
+        if media is not None:
             break
     else:
         await msg.edit('This is not supported file format')
         return
+    
+    file_id, file_ref = unpack_new_file_id(media.file_id)
 
     result = await Media.collection.delete_one({
-        'file_name': media.file_name,
-        'file_size': media.file_size,
-        'file_type': media.file_type,
-        'mime_type': media.mime_type
+        '_id': file_id,
     })
-
     if result.deleted_count:
         await msg.edit('File is successfully deleted from database')
     else:
-        await msg.edit('File not found in database')
+        file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
+        result = await Media.collection.delete_many({
+            'file_name': file_name,
+            'file_size': media.file_size,
+            'mime_type': media.mime_type
+            })
+        if result.deleted_count:
+            await msg.edit('File is successfully deleted from database')
+        else:
+            # files indexed before https://github.com/EvamariaTG/EvaMaria/commit/f3d2a1bcb155faf44178e5d7a685a1b533e714bf#diff-86b613edf1748372103e94cacff3b578b36b698ef9c16817bb98fe9ef22fb669R39 
+            # have original file name.
+            result = await Media.collection.delete_many({
+                'file_name': media.file_name,
+                'file_size': media.file_size,
+                'mime_type': media.mime_type
+            })
+            if result.deleted_count:
+                await msg.edit('File is successfully deleted from database')
+            else:
+                await msg.edit('File not found in database')
